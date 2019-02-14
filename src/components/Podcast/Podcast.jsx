6dca1e79fa601a3f99a114d641e146d630396e2f
Phil Hagger <philip.hagger@gmail.com>
2019-02-14 17:04:23 +0000

    - applied media queries to the podcast boxes

--- src/components/Podcast/Podcast.jsx
+++ src/components/Podcast/Podcast.jsx
@@ -11,18 +11,27 @@ const Podcast = ({
   },
   ...props
 }) => (
-  <div className="story">
-    <figure className="story__shape">
-      <img src={require(`../../img/people/${imageURL}`)} alt={name} className="story__image" />
-      <figcaption className="story__caption">{name}</figcaption>
+  <div className='story'>
+    <figure className='story__shape'>
+      <img
+        src={require(`../../img/people/${imageURL}`)}
+        alt={name}
+        className='story__image'
+      />
+      <figcaption className='story__caption'>{name}</figcaption>
     </figure>
-    <div className="story__text">
-      <h3 className="heading-tertiary">{title}</h3>
-      <h5 className="date-text">{moment(date).format('ddd, Do MMM YY')}</h5>
-      <p>{props.children}</p>
+    <div className='story__text'>
+      <h3 className='heading-tertiary'>{title}</h3>
+      <h5 className='date-text'>{moment(date).format('ddd, Do MMM YY')}</h5>
+      <p className='description'>{props.children}</p>
     </div>
-    <div className="story__audio">
-      <audio className="audio" src={`podcasts/${url}`} type="audio/mp3" controls />
+    <div className='story__audio'>
+      <audio
+        className='audio'
+        src={`podcasts/${url}`}
+        type='audio/mp3'
+        controls
+      />
     </div>
   </div>
 );
